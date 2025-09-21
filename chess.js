14dadcdb5a346c9300889425085a03d6d8391c41
Nikita-Babich <nikita.babich.a@gmail.com>
2025-09-21 22:21:01 +0300

    drawing board

--- chess.js
+++ chess.js
@@ -1,6 +1,26 @@
-const canvas = document.getElementById('myCanvas');
+const canvas = document.getElementById('ChessBoard');
 const ctx = canvas.getContext('2d');
+const width = canvas.width;
+const height = canvas.height;
 
-// drawing one figure
-ctx.fillStyle = 'red';
-ctx.fillRect(10, 10, 100, 50);
+function drawSquare(i,j,width){
+ctx.fillStyle = '#CCCC00';
+ctx.fillRect(i*width/8, j*width/8, width/8, width/8);
+}
+
+function FillChessBackground(){
+    const width = canvas.width;
+    //const height = canvas.height;
+    ctx.fillStyle = '#FFFF00';
+    ctx.fillRect(0, 0, width, width); //placement to be changed
+    const squareSize = width/8;
+    for(let i=0; i<8; i++){
+        for(let j=0; j<8; j++){
+            if((i+j)%2==1){
+                drawSquare(i,j,width);
+            };
+        }
+    }
+}
+
+FillChessBackground()
