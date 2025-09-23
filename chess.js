f9d3572c0ad71a649883ddfad6d9789d2226b694
AAArtLeG <art.legeida@gmail.com>
2025-09-23 18:31:17 +0200

    add onclick event func with inputted text
    1)add input html element, button html element, h1 html element
    
    2) func that triggered when you click on the button. Func grab 'value' that user put in the input elem, then send it in console + add this text to a h1

--- chess.js
+++ chess.js
@@ -1,26 +1,33 @@
-const canvas = document.getElementById('ChessBoard');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("ChessBoard");
+const ctx = canvas.getContext("2d");
 const width = canvas.width;
 const height = canvas.height;
 
-function drawSquare(i,j,width){
-ctx.fillStyle = '#CCCC00';
-ctx.fillRect(i*width/8, j*width/8, width/8, width/8);
+function drawSquare(i, j, width) {
+  ctx.fillStyle = "#CCCC00";
+  ctx.fillRect((i * width) / 8, (j * width) / 8, width / 8, width / 8);
 }
 
-function FillChessBackground(){
-    const width = canvas.width;
-    //const height = canvas.height;
-    ctx.fillStyle = '#FFFF00';
-    ctx.fillRect(0, 0, width, width); //placement to be changed
-    const squareSize = width/8;
-    for(let i=0; i<8; i++){
-        for(let j=0; j<8; j++){
-            if((i+j)%2==1){
-                drawSquare(i,j,width);
-            };
-        }
+function FillChessBackground() {
+  const width = canvas.width;
+  //const height = canvas.height;
+  ctx.fillStyle = "#FFFF00";
+  ctx.fillRect(0, 0, width, width); //placement to be changed
+  const squareSize = width / 8;
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if ((i + j) % 2 == 1) {
+        drawSquare(i, j, width);
+      }
     }
+  }
 }
 
-FillChessBackground()
+FillChessBackground();
+
+document.getElementById("buttonForTakeTextFromFname").onclick = function () {
+  const inputTextElem = document.getElementById("fname");
+  console.log(inputTextElem.value);
+  const header1Forname = document.getElementById("myName");
+  header1Forname.innerHTML = inputTextElem.value;
+};
